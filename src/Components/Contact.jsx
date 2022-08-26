9538be9cbc98e352873b50a238e9fa2406256bd3
Mussafara Riaz <mussafarariaz@gmail.com>
2022-08-26 21:33:26 +0500

    commit

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Contact() {
   return (
-    <div className='mx-auto'>
+    <div className='mx-auto my-auto'>
      <div class="input-group mb-3">
   <span class="input-group-text" id="basic-addon1">@</span>
   <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1"/>
